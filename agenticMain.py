2dd12cd4d2bc14c15744186267749352dabc3ca1
PhuPwintEaindray5298 <phoopwint5298@gmail.com>
2026-05-16 23:55:47 +0100

    Corrective RAG Implement

--- agenticMain.py
+++ agenticMain.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from agentic_graph.graph import graph
+
 if __name__ == '__main__':
     print("Hello Agentic RAG")
+    print(graph.invoke(input={"question":"what is agent memory?"}))
